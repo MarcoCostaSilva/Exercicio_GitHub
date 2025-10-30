89c3f43d44dfb782d11b4a0ae5bc104771abddff
Marco Aurélio Costa da Silva <marco.dev.data@gmail.com>
2025-10-30 20:02:58 -0300

    Adiciona validação de login e mensagens de erro

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const senha = document.getElementById("senha");
 const mensagem = document.getElementById("mensagem");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (usuario.value === "" || senha.value === "") {
-    mensagem.textContent = "Preencha todos os campos!";
-    mensagem.style.color = "#ff5252";
-  } else if (usuario.value === "adm" && senha.value === "123") {
-    mensagem.textContent = "Login realizado com sucesso!";
-    mensagem.style.color = "#03dac6";
-  } else {
-    mensagem.textContent = "Usuário ou senha incorretos.";
-    mensagem.style.color = "#ff5252";
-  }
+    e.preventDefault();
+    if (usuario.value === "" || senha.value === "") {
+        mensagem.textContent = "Preencha todos os campos!";
+        mensagem.style.color = "#ff5252";
+    } else if (usuario.value === "adm" && senha.value === "123") {
+        mensagem.style.color = "#03dac6";
+        mensagem.textContent = "Login realizado com sucesso!";
+    } else {
+        mensagem.textContent = "Usuário ou senha incorretos.";
+        mensagem.style.color = "#ff5252";
+    }
 });
